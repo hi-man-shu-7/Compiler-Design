5df2ed9674d3ad13d933e7a5939474749b1db72b
Himanshu Nainwal <114490861+hi-man-shu-7@users.noreply.github.com>
2025-02-03 21:00:36 +0530

    Add files via upload

---
+++ Ex. 2/vans.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// create a class
+class Room {
+
+   public:
+   double length;
+   double breadth;
+   double height;
+
+   double calculate_area() {
+      return length * breadth;
+   }
+
+   double calculate_volume() {
+      return length * breadth * height;
+   }
+};
